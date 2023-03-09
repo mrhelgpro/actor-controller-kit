3009ab0ead4ded1532adbb97e4d44ead43f7d509
Oleg Kolisnichenko <mrhelgpro@gmail.com>
2023-03-09 16:25:55 +0200

    New Movable

--- AssemblyActorExtentions.cs
+++ AssemblyActorExtentions.cs
@@ -11,27 +11,5 @@ namespace AssemblyActorCore
 
             return gameObject.GetComponent<T>() == null ? gameObject.AddComponent<T>() : gameObject.GetComponent<T>();
         }
-
-        public static void Move(this Transform transform, Vector3 velocity, float speed)
-        {
-            transform.position += velocity * speed;
-        }
-
-        public static void Move(this Rigidbody rigidbody, Vector3 velocity, float speed, float gravity = 1.0f)
-        {
-            rigidbody.isKinematic = gravity == 0;
-            rigidbody.useGravity = false;
-            rigidbody.MovePosition(rigidbody.position + velocity * speed);
-            rigidbody.AddForce(Physics.gravity * gravity, ForceMode.Acceleration);
-        }
-
-        public static void Move(this Rigidbody2D rigidbody, Vector3 velocity, float speed, float gravity = 1.0f)
-        {
-            float horizontal = velocity.x * speed * 20;
-            float vertical = gravity == 0 ? 0 : rigidbody.velocity.y;
-            rigidbody.bodyType = gravity == 0 ? RigidbodyType2D.Kinematic : RigidbodyType2D.Dynamic;
-            rigidbody.gravityScale = gravity;
-            rigidbody.velocity = new Vector2(horizontal, vertical);
-        }
     }
 }
--- AssemblyModelKit.cs
+++ AssemblyModelKit.cs
@@ -4,9 +4,10 @@ namespace AssemblyActorCore
 {
     public class Movable : MonoBehaviour
     {
-        [Range(0, 1)] public float Slowing;
-        [Range(0, 10)] public float Acceleration;
-        private float _getSpeed => _getAcceleration * _getSlowing * Time.fixedDeltaTime * 10;
+        [Range(0, 1)] public float Slowing = 0;
+        [Range(0, 10)] public float Acceleration = 0;
+        [Range(-1, 1)] public float Gravity = 1;
+        private float _speedScale => _getAcceleration * _getSlowing * Time.fixedDeltaTime;
         private float _getSlowing => Slowing > 0 ? (Slowing <= 1 ? 1 - Slowing : 0) : 1;
         private float _getAcceleration => Acceleration > 0 ? Acceleration + 1 : 1;
 
@@ -21,7 +22,6 @@ namespace AssemblyActorCore
             Actor actor = GetComponentInParent<Actor>();
 
             _mainTransform = actor.gameObject.transform;
-
             _mode = actor.Mode;
 
             switch (_mode)
@@ -35,20 +35,59 @@ namespace AssemblyActorCore
             }
         }
 
-        public void SetMovement(Vector3 velocity, float gravity = 1.0f)
+        public void MoveByVelocity(Vector3 direction, float speed)
         {
             switch (_mode)
             {
                 case EnumMode.Free:
-                    _mainTransform.Move(velocity, _getSpeed);
+                    VelocityFree(direction, speed);
                     break;
                 case EnumMode.ThirdPerson:
-                    _rigidbody.Move(velocity, _getSpeed, gravity);
+                    VelocityThirdPerson(direction, speed);
                     break;
                 case EnumMode.Platformer:
-                    _rigidbody2D.Move(velocity, _getSpeed, gravity);
+                    VelocityPlatformer(direction, speed);
                     break;
-            }  
+            }
+        }
+
+        public void MoveToPosition(Vector3 direction, float speed)
+        {
+            if (_mode == EnumMode.ThirdPerson)
+            {
+                _rigidbody.useGravity = false;
+            }
+            else if (_mode == EnumMode.Platformer)
+            {
+                _rigidbody2D.gravityScale = 0;
+            }
+
+            direction.Normalize();
+            _mainTransform.position += direction * speed * Time.fixedDeltaTime;
+        }
+
+        private void VelocityFree(Vector3 direction, float speed)
+        {
+            direction.Normalize();
+
+            _mainTransform.position += direction * speed * _speedScale;
+        }
+
+        private void VelocityThirdPerson(Vector3 direction, float speed)
+        {
+            direction.Normalize();
+
+            _rigidbody.useGravity = false;
+            _rigidbody.MovePosition(_rigidbody.position + direction * speed * _speedScale);
+            _rigidbody.AddForce(Physics.gravity * Gravity, ForceMode.Acceleration);
+        }
+
+        private void VelocityPlatformer(Vector3 direction, float speed)
+        {
+            direction.Normalize();
+
+            _rigidbody2D.gravityScale = Gravity;
+            _rigidbody2D.velocity = direction * speed * _speedScale;
         }
     }
 
