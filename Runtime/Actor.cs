06cf28c8210201ef8247d580956c93383ca057a0
mrhelgpro <mrhelgpro@gmail.com>
2023-03-26 22:30:31 +0300

    UPD

--- Runtime/Actor.cs
+++ Runtime/Actor.cs
@@ -142,7 +142,7 @@ namespace AssemblyActorCore
             NavMeshAgent navMeshAgent = gameObject.AddThisComponent<NavMeshAgent>();
             navMeshAgent.agentTypeID = 0;
             navMeshAgent.radius = 0.25f;
-            navMeshAgent.height = 1.25f;
+            navMeshAgent.height = navMeshAgent.radius * 2;
             navMeshAgent.acceleration = 100;
             navMeshAgent.angularSpeed = 10000;
         }
