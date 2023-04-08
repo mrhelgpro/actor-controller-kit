64bd6f0bc077ed8042a159bcb4877ccc942904ca
mrhelgpro <mrhelgpro@gmail.com>
2023-04-08 10:10:12 +0300

    Default <Action> - to deal damage 2.1

--- Runtime/DamageByTrigger.cs
+++ Runtime/DamageByTrigger.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 namespace AssemblyActorCore
 {
@@ -11,7 +12,7 @@ namespace AssemblyActorCore
 
 		private void Awake() => Layer = LayerMask.GetMask("Damagable");
 
-		public override void OnTargetEnter(Transform target)
+        public override void OnTargetEnter(Transform target)
 		{
 			Damagable damagable = target.GetComponent<Damagable>();
 
@@ -33,14 +34,30 @@ namespace AssemblyActorCore
 
 				_targets.Add(target);
 				damagable.TakeDamage(Value);
+
+				Invoke(nameof(refresh), 1);
 			}
 		}
 
-		public override void OnTargetExit(Transform target)
-		{
+		public override void OnTargetExit(Transform target) { }
 
-		}
+		private void OnDisable() => refresh();
 
-        private void OnDisable() => _targets.Clear();
+		private void refresh() => _targets.Clear();
 	}
+
+#if UNITY_EDITOR
+	[ExecuteInEditMode]
+	[CustomEditor(typeof(DamageByTrigger))]
+	public class DamageByTriggerEditor : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			DamageByTrigger myTarget = (DamageByTrigger)target;
+
+			myTarget.Value = EditorGUILayout.FloatField("Value", myTarget.Value);
+			myTarget.Tag = EditorGUILayout.TextField("Tag", myTarget.Tag);
+		}
+	}
+#endif
 }
